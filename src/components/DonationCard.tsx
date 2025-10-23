0994f7c148d620fe6ba3bb28dc5583b49223ec75
devolos <utk27032005@gmail.com>
2025-10-23 18:52:46 +0530

    mc

--- src/components/DonationCard.tsx
+++ src/components/DonationCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+import Link from "next/link";
 import { FaHeart } from "react-icons/fa";
 import SpotlightCard from "./SpotlightCard";
 import { Button } from "./ui/button";
@@ -8,17 +8,10 @@ import { CardDescription, CardHeader, CardTitle } from "./ui/card";
 
 export default function DonationCard() {
   return (
-    <SpotlightCard className="">
-      <div className="">
-        <Image
-          src="/qrcode.png" // replace with your QR image path
-          alt="Donation QR Code"
-          width={300}
-          height={160}
-          className="rounded-lg border border-gray-200 shadow-md"
-        />
+    <SpotlightCard>
+      <div>
         <CardHeader className="p-0">
-          <CardTitle className="text-2xl mt-3 font-bold tracking-tight flex items-center gap-2">
+          <CardTitle className="text-2xl font-bold tracking-tight flex items-center gap-2">
             <FaHeart className="animate-pulse text-pink-400" />
             Support the dev
           </CardTitle>
@@ -37,7 +30,17 @@ export default function DonationCard() {
         </CardHeader>
         <div className="mt-5 flex justify-between">
           <Button size="lg" className="w-full">
-            Donate via Google Pay
+            <a
+              href="upi://pay?pa=spryzenutkarsh@oksbi&pn=Kumar Utkarsh"
+              target="_blank"
+            >
+              Donate via Google Pay
+            </a>
+          </Button>
+        </div>
+        <div className="mt-2 flex justify-between">
+          <Button size="lg" className="w-full">
+            <Link href={"/plans"}>Donate via Dodo Payments</Link>
           </Button>
         </div>
       </div>
