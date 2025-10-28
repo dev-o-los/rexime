dc85d945151b9d98d66ab267bf55898548179335
devolos <utk27032005@gmail.com>
2025-10-28 08:07:57 +0530

    logo cloud added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,30 @@
 import Hero03 from "@/components/hero-03/hero-03";
+import LogoCloud from "@/components/logos-07/LogoCloud";
 
 export default async function Home() {
   return (
-    <div className="h-screen w-full relative">
-      {/* <div className="absolute">sss</div> */}
-      <Hero03 />
-
-      {/* <LiquidEther
-        colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
-        mouseForce={20}
-        cursorSize={100}
-        isViscous={false}
-        viscous={30}
-        iterationsViscous={32}
-        iterationsPoisson={32}
-        resolution={0.5}
-        isBounce={false}
-        autoDemo={true}
-        autoSpeed={0.5}
-        autoIntensity={2.2}
-        takeoverDuration={0.25}
-        autoResumeDelay={3000}
-        autoRampDuration={0.6}
-      /> */}
+    <div>
+      <div className="h-screen w-full relative">
+        <Hero03 />
+        {/* <LiquidEther
+          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        /> */}
+      </div>
+      <LogoCloud />
     </div>
   );
 }
