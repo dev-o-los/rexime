eb355347080ef81193460d60005eada8b8e5cbaa
devolos <utk27032005@gmail.com>
2025-11-12 07:29:00 +0530

    Update resumeAtom to use DUMMY_STANDARD_DATA and add new resume templates in TemplateShowcase

--- src/components/builder/middle-section/TemplateShowcase.tsx
+++ src/components/builder/middle-section/TemplateShowcase.tsx
@@ -5,6 +5,10 @@ import {
   resumeShowCaseIdxAtom,
   selectedFontAtom,
 } from "@/app/store";
+import ResumeCreativeProfessional from "@/components/templates/ResumeCreativeProfessional";
+import ResumeModernCorporate from "@/components/templates/ResumeModernCorporate";
+import ResumeStudentEntry from "@/components/templates/ResumeStudentEntry";
+import ResumeTechOriented from "@/components/templates/ResumeTechOriented";
 import { useAtomValue } from "jotai";
 import { useRef } from "react";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
@@ -26,6 +30,10 @@ export default function TemplateShowcase() {
     1: <ResumeBerlin data={data} font={font} ref={pdfRef} />,
     2: <ResumeTimeLine data={data} font={font} ref={pdfRef} />,
     3: <ResumeAmsterdam data={data} font={font} ref={pdfRef} />,
+    4: <ResumeCreativeProfessional data={data} font={font} ref={pdfRef} />,
+    5: <ResumeModernCorporate data={data} font={font} ref={pdfRef} />,
+    6: <ResumeTechOriented data={data} font={font} ref={pdfRef} />,
+    7: <ResumeStudentEntry data={data} font={font} ref={pdfRef} />,
   };
 
   return (
