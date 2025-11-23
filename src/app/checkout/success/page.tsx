541a3e58bc7950740db49a4f8ad7fbb032ab90af
devolos <utk27032005@gmail.com>
2025-11-23 11:20:28 +0530

    payment status updated

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -10,19 +10,36 @@ function PaymentStatus() {
   const params = useSearchParams();
   const status = params.get("status");
   const isFailed = status === "failed";
+  const isProcessing = status === "processing";
 
   return (
     <div
-      className={`grid place-content-center h-screen ${ibmplexmono.className} text-[max(2vw,1.2rem)]`}
+      className={`grid place-content-center h-screen ${ibmplexmono.className} text-[max(2vw,1.2rem)] mx-7`}
     >
       <div className="text-center">
-        <div className={isFailed ? "text-destructive" : "text-chart-2"}>
-          Payment {isFailed ? "Failed" : "Successful"}
+        <div
+          className={
+            isFailed
+              ? "text-destructive"
+              : isProcessing
+              ? "text-amber-300"
+              : "text-chart-2"
+          }
+        >
+          Payment{" "}
+          {isFailed ? "Failed" : isProcessing ? "Processing" : "Successful"}
         </div>
+        {isProcessing && (
+          <div className="text-sm py-5">
+            Your bank is taking longer than usual to verify the transaction. If
+            this doesn’t complete, please try another card or enable online
+            payments.
+          </div>
+        )}
         {isFailed ? "Please try again later" : "Thank you for donating"}
       </div>
-      <Link href="/">
-        <Button className="mt-2 w-full">Navigate to home</Button>
+      <Link className="text-center" href="/">
+        <Button className="mt-2  w-full max-w-[500px]">Navigate to home</Button>
       </Link>
     </div>
   );
